451b5151475be147770581aaa9280732f0c62f33
Gabriel Solomon <hello@gabrielsolomon.ro>
2019-04-28 19:14:15 +0300

    fix getenv

--- tests/fixtures/enviromentParams.php
+++ tests/fixtures/enviromentParams.php
@@ -2,6 +2,6 @@
 
 $parameters = isset($parameters) ? $parameters : [];
 foreach (['terminal', 'merchant_name', 'merchant_url', 'merchant_email', 'key'] as $field) {
-    $parameters[$field] = $_ENV['ROMCARD_' . strtoupper($field)];
+    $parameters[$field] = getenv('ROMCARD_' . strtoupper($field));
 }
 return $parameters;
